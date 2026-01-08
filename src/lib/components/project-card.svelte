fa1998e150c6e77fcb59a7bfcb7f4d91411d0ee0
Arden <arden.haldorson@gmail.com>
2026-01-08 13:22:49 +0100

    created product card component and implemented it on the landing page

---
+++ src/lib/components/project-card.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	interface Props {
+		className?: string;
+		linkName: string;
+        imgSrc: string;
+		imgAlt: string;
+        projectTitle: string;
+	}
+
+	let {
+		className,
+		linkName,
+        imgSrc,
+		imgAlt,
+        projectTitle
+
+	}: Props = $props();
+
+</script>
+
+<a href={linkName} class="group border-3 border-slate-400 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 relative overflow-hidden h-44 {className}">
+	<img src="/project-images/thumbnails/{imgSrc}" alt={imgAlt} class="object-cover"/>
+    <div class="bg-slate-900 opacity-80 absolute inset-0 group-hover:opacity-25 transition-opacity duration-300"></div>
+    <h3 class="rokkitt-font font-bold text-amber-600 text-xl tracking-wider absolute bottom-0 right-0 m-3 mb-2 group-hover:bg-slate-700 group-hover:rounded-xl group-hover:px-2 group-hover:py-1 group-hover:mb-1 group-hover:mr-1 group-hover:text-slate-200">{projectTitle}</h3>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,41 @@
-<section class="flex flex-col items-center p-8">
-    <div class="flex items-center gap-4 mb-8">
-        <div>
-            <h1 class="text-5xl">Hi</h1>
-            <p class="text-sm">My name is Arden. I'm a fullstack developer. </p>
+<script lang="ts">
+    import ProjectCard from '$lib/components/project-card.svelte';
+</script>
+
+<section class="flex flex-col items-center p-8 gap-8 lg:mb-4">
+        <div class="flex items-center justify-center gap-8">
+            <img src="/arden-logo-1.svg" alt="Arden Logo" class="h-52 ml-2 lg:h-64" />
+            <h1 class="text-6xl rokkitt-font font-bold flex flex-col lg:flex-row lg:gap-4 tracking-wider text-slate-400">
+                <span>Hello</span>
+                <span>there</span>
+            </h1>
         </div>
-        <img src="/arden-logo-1.svg" alt="Arden Logo" class="h-52" />
+        <p class="text-[1.15rem] rokkitt-font-regular tracking-wider text-center text-slate-200">My name is Arden and I am a fullstack developer. <br /> Welcome to my portfolio.</p>
+</section>
+
+<section class="flex flex-col items-center">
+    <h2 class="text-3xl rokkitt-font font-bold tracking-wider text-amber-600">Latest Projects</h2>
+        <!-- Project Cards will go here -->
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-6">
+            <!-- Example Project Card -->
+            <ProjectCard
+                linkName="/projects/arden-reads"
+                imgSrc="arden-reads-tn.png"
+                imgAlt="Arden Reads Thumbnail"
+                projectTitle="Arden Reads"
+            />
+            <ProjectCard
+                linkName="/projects/casino-nova"
+                imgSrc="casino-nova-tn.png"
+                imgAlt="Casino Nova Thumbnail"
+                projectTitle="Casino Nova"
+            />
+            <ProjectCard
+                linkName="/projects/munamii-cakery"
+                imgSrc="munamii-cakery-tn.png"
+                imgAlt="Munamii Cakery Thumbnail"
+                projectTitle="Munamii Cakery"
+            />
     </div>
+
 </section>
